f720b6aa07ecb0035e59d90d6c789e1d4322d835
swapnilkothawade <swapnil7080@gmail.com>
2019-07-23 20:30:14 -0400

    first commit

---
+++ routes/user.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const {
+    userById,
+    allUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+    userPhoto,
+    addFollowing,
+    addFollower,
+    removeFollowing,
+    removeFollower,
+    findPeople,
+    hasAuthorization,
+    addAsChildren,
+    addParent
+} = require("../controllers/user");
+const { requireSignin } = require("../controllers/auth");
+
+const router = express.Router();
+
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
+router.put("/user/addaschild", requireSignin, addParent, addAsChildren);
+
+router.get("/users", allUsers);
+router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, hasAuthorization, updateUser);
+router.delete("/user/:userId", requireSignin, hasAuthorization, deleteUser);
+// photo
+router.get("/user/photo/:userId", userPhoto);
+
+// who to follow
+router.get("/user/findpeople/:userId", requireSignin, findPeople);
+
+// any route containing :userId, our app will first execute userByID()
+router.param("userId", userById);
+
+module.exports = router;
